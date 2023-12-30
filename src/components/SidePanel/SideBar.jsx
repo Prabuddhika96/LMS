733267f2513d3cbfba83214af978aaa29379f0ca
Prabuddhika96 <prabuddhika1996@gmail.com>
2023-12-31 03:47:30 +0530

    create nav bar

--- src/components/SidePanel/SideBar.jsx
+++ src/components/SidePanel/SideBar.jsx
@@ -1,4 +1,42 @@
 import React, { useEffect, useRef, useState } from "react";
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+} from "@ant-design/icons";
+import { Button, Menu } from "antd";
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("Option 3", "3", <ContainerOutlined />),
+  getItem("Navigation One", "sub1", <MailOutlined />, [
+    getItem("Option 5", "5"),
+    getItem("Option 6", "6"),
+    getItem("Option 7", "7"),
+    getItem("Option 8", "8"),
+  ]),
+  getItem("Navigation Two", "sub2", <AppstoreOutlined />, [
+    getItem("Option 9", "9"),
+    getItem("Option 10", "10"),
+    getItem("Submenu", "sub3", null, [
+      getItem("Option 11", "11"),
+      getItem("Option 12", "12"),
+    ]),
+  ]),
+];
 
 function SideBar({ isActive, navData, setIsActive }) {
   const [iconClicked, setIconClicked] = useState(false);
@@ -24,18 +62,20 @@ function SideBar({ isActive, navData, setIsActive }) {
   }, [isActive]);
 
   return (
-    <>
-      <div
-        className={`duration-500 !bg-white sidebar z-40 navbar fixed overflow-y-auto text-center shadow h-screen max-sm:z-[100] max-sm:mt-0 ${
-          isActive
-            ? "left-0 p-2 w-[200px] !overflow-visible max-sm:w-[100px] "
-            : " md:left-[0px] left-[20px] p-2 w-[50px] "
-        }`}
-        ref={popRef}
-      >
-        {/* Rest of your content */}
-      </div>
-    </>
+    <div
+      className={`${
+        isActive ? `w-[200px]` : `w-[80px]`
+      } h-screen duration-500 bg-white`}
+    >
+      <Menu
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+        theme="light"
+        inlineCollapsed={!isActive}
+        items={items}
+      />
+    </div>
   );
 }
 
