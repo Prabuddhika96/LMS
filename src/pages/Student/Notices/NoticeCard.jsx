2436b5693948ae9e622dc5c485c277df2a6a9af3
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-01-03 11:55:32 +0530

    hide notice more btn

--- src/pages/Student/Notices/NoticeCard.jsx
+++ src/pages/Student/Notices/NoticeCard.jsx
@@ -7,11 +7,6 @@ function NoticeCard({ notice }) {
       <Card
         type="inner"
         title={notice.title}
-        extra={
-          <a href="#" className="text-[var(--main-blue)]">
-            More
-          </a>
-        }
         className="my-3 hover:shadow-lg duration-200 ease-in-out"
       >
         {notice.notice}
